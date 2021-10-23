dead9369640d3b53d8a22d1acc953714a29629f1
ujjwalsainiT <ujjwalreactjsdeveloper@gmail.com>
2021-10-23 09:36:45 -0700

    ujjwal

--- src/Components/SuperAdmin/Subscription/Subscription.jsx
+++ src/Components/SuperAdmin/Subscription/Subscription.jsx
@@ -1,14 +1,268 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Card, Button, Dialog, DialogActions, DialogTitle, DialogContent } from '@material-ui/core';
+import Expand from "react-expand-animated";
 
 //common header
 import HOC from "../../../Common/HOC";
 
+import "./Subscription.css";
+
 function Subscription() {
+
+    //local state
+    const [addMangeopen, setaddMangeopen] = useState(false);
+    const [name, setname] = useState("");
+    const [Month, setMonth] = useState("");
+    const [SubscriptionDataArr, setSubscriptionDataArr] = useState([]);
+    const [EditDailogOpen, setEditDailogOpen] = useState(false);
+    const [Editname, setEditname] = useState("");
+    const [EditMonth, setEditMonth] = useState("")
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+
+
+    const OpenEditDailog = (data) => {
+        setEditname(data.name);
+        setEditMonth(data.month);
+        setEditDailogOpen(!EditDailogOpen)
+    }
     return (
         <>
             <div className="content_padding">
-                subscription
+
+                <div className="mb-3 page_heading">Manage Subscription</div>
+                <Card className="pt-3 pb-4 Card_shadow">
+                    <div className="card_admissiondetails_height">
+                        <div className="textfiled_margin">
+                            {!addMangeopen ? (
+                                <div className="d-flex">
+                                    <span className="icon_color">
+                                        <i className="fa fa-plus-circle"></i>
+                                    </span>
+                                    <span className="mt-1 ml-2 addmanageuserfont hover_cursor" onClick={() => setaddMangeopen(!addMangeopen)}>
+                                        <strong> Add New Subscription</strong>
+                                    </span>
+                                </div>
+                            ) : (
+                                <Expand open={addMangeopen}>
+                                    <Card className=" mb-2 Card_shadow">
+                                        <div className="card_admissiondetails_height">
+                                            <div className="textfiled_margin">
+                                                <div className="card_content_instition">
+                                                    <div
+                                                        className="text-right"
+                                                        onClick={() => setaddMangeopen(!addMangeopen)}
+                                                    >
+                                                        <span className="icon_color">
+                                                            <i class="fa fa-times cursor"></i>
+                                                        </span>
+                                                    </div>
+                                                    <div className="text_filed_heading">
+                                                        Subscription Name
+                                                    </div>
+                                                    <div className=" mt-1">
+                                                        <input
+                                                            type="text"
+                                                            className="form-control "
+                                                            placeholder="Enter the Subscription Name"
+                                                            autoComplete="off"
+                                                            value={name}
+                                                            onChange={(e) => {
+                                                                setname(e.target.value);
+                                                            }}
+                                                        />
+                                                    </div>
+
+                                                    <div className="text_filed_heading">
+                                                        Month
+                                                    </div>
+                                                    <div className=" mt-1">
+                                                        <input
+                                                            type="text"
+                                                            className="form-control "
+                                                            placeholder="Enter Month"
+                                                            autoComplete="off"
+                                                            value={Month}
+                                                            onChange={(e) => {
+                                                                const re = /^[0-9\b]+$/;
+                                                                if (e.target.value === '' || re.test(e.target.value)) {
+                                                                    setMonth(e.target.value);
+                                                                }
+                                                            }}
+                                                        />
+                                                    </div>
+
+                                                </div>
+                                                <div className="mt-2 pb-2 ">
+                                                    <Button
+                                                        variant="contained"
+                                                        className="button_formatting"
+                                                        onClick={() => {
+                                                            if (name === "") {
+                                                                alert("Enter the Subscription Name");
+                                                                return;
+                                                            }
+                                                            if (Month === "") {
+                                                                alert("Enter the Month");
+                                                                return;
+                                                            }
+                                                            SubscriptionDataArr.push({
+                                                                name: name,
+                                                                month: Month,
+                                                                show: true,
+                                                            });
+                                                            setSubscriptionDataArr([...SubscriptionDataArr]);
+                                                            setname("");
+                                                            setMonth("");
+                                                        }}
+                                                    >
+                                                        Create
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Card>
+                                </Expand>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="card_admissiondetails_height mt-4">
+                        <div className="textfiled_margin cardheight_overflow">
+                            {/* <div className="d-flex justify-content-between">
+                                <div className=" mt-1 mb-1">
+                                    <strong> Subsciption Name</strong>
+                                </div>
+
+                                <div className=" mt-1 mb-1">
+                                    <strong> Month</strong>
+                                </div>
+
+                                {" "}
+                                <div className="p-2">
+                                    <strong> Action</strong>
+                                </div>
+
+                            </div> */}
+                            <hr />
+                            {SubscriptionDataArr.length > 0 ?
+                                (SubscriptionDataArr.map((item, index) => (
+                                    <Card className="Card_shadow mb-2 mt-2">
+                                        <div className="card_admissiondetails_height">
+                                            <div className="textfiled_margin">
+                                                <div className="d-flex justify-content-between">
+
+                                                    <div className=" p-2">
+                                                        {item.name}
+                                                    </div>
+
+                                                    <div className="text-center p-2">
+                                                        {item.month}
+                                                    </div>
+
+                                                    {" "}
+                                                    <div className="d-flex p-2">
+
+                                                        <span className="action_icon mr-2 ml-1">
+                                                            <i
+                                                                className="fa fa-pencil"
+                                                                onClick={() => OpenEditDailog(item)}
+                                                            ></i>
+                                                        </span>
+                                                        <span className="action_icon ml-2">
+                                                            <i
+                                                                className="fa fa-trash "
+                                                                onClick={() => {
+                                                                    SubscriptionDataArr.splice(index, 1);
+                                                                    setSubscriptionDataArr([...SubscriptionDataArr]);
+                                                                }}
+                                                            ></i>
+                                                        </span>
+                                                        <span className="action_icon  ml-2">
+                                                            Manage
+                                                        </span>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Card>
+                                )))
+                                : (
+                                    <span>No Data</span>
+                                )}
+                        </div>
+                    </div>
+                </Card>
             </div>
+
+            <Dialog
+                open={EditDailogOpen}
+                aria-labelledby="form-dialog-title"
+                maxWidth="md"
+                fullWidth="fullWidth"
+            >
+                <DialogTitle>
+                    Edit Subscription
+                    <span
+                        className="float-right icon_color"
+                        onClick={() => setEditDailogOpen(!EditDailogOpen)}
+                    >
+                        <i class="fa fa-times hover_cursor" aria-hidden="true"></i>{" "}
+                    </span>
+                </DialogTitle>
+                <DialogContent>
+                    <div className="text_filed_heading">
+                        Number of Bids
+                    </div>
+                    <div className=" mt-1">
+                        <input
+                            type="text"
+                            className="form-control "
+                            placeholder="Enter Bids"
+                            autoComplete="off"
+                            value={Editname}
+                            onChange={(e) => {
+                                setEditname(e.target.value);
+                            }}
+                        />
+                    </div>
+
+                    <div className="text_filed_heading">
+                        Price
+                    </div>
+                    <div className=" mt-1">
+                        <input
+                            type="text"
+                            className="form-control "
+                            placeholder="Enter Price"
+                            autoComplete="off"
+                            value={EditMonth}
+                            onChange={(e) => {
+                                const re = /^[0-9\b]+$/;
+                                if (e.target.value === '' || re.test(e.target.value)) {
+                                    setEditMonth(e.target.value);
+                                }
+                            }}
+                        />
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        className="button_formatting"
+                        onClick={() => setEditDailogOpen(!EditDailogOpen)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        className="button_formatting"
+                    >
+                        Save{" "}
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
